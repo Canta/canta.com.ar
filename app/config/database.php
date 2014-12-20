2fe1da109ea9ade4d69186e7377cd8e6a4294418
Daniel Cantarín <omega_canta@yahoo.com>
2014-12-20 19:44:37 -0300

    Updated the DB data in Laravel's config.

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'canta',
+			'username'  => 'admin_canta',
+			'password'  => 'PASS_ADMIN_HERE',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
